4aa35407bc6c07a36c410c1507f0cfca645c1b1a
Agate1234 <faizabiyu72@gmail.com>
2024-08-29 17:31:46 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>KTP</title>
+        <style>
+            .container{
+                margin: 0;
+                padding: 0;
+                text-align: center;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+
+            }
+
+            table{
+                height: 450px;
+                width: 850px;
+                margin: auto;
+                padding-bottom: 75px;
+                border-radius: 2em;
+            }
+
+            td{
+                padding-left: 50px;
+                font-size: 20px;
+            }
+        </style>
+    </head>
+    <body>
+        <h1 align="center">WELCOME<br><br>BIODATA DIRI</h1>
+        <table>
+            <div class = container>
+                <div>
+                    <tr class = td>
+                        <td><font size = 5 face = courier>NIK</font></td>
+                        <td><font size = 5 face = courier colspan = 3>: 350******</font></td>
+                    </tr>
+                </div>
+                <tr>
+                    <td>Nama</td>
+                    <td colspan = 2>: FA'IZ ABIYU ATHA FAWAS</td>
+                </tr>
+                <tr>
+                    <td>Tempat/tgl Lahir</td>
+                    <td colspan = 2>: BOJONEGORO, 02-09-2004</td>
+                </tr>
+                <tr>
+                    <td>Jenis Kelamin</td>
+                    <td>: LAKI-LAKI</td>
+                    <td align = left>Gol Darah : -</td>
+                </tr>
+                <tr>
+                    <td>Alamat</td>
+                    <td colspan = 2>: JL. PARE BARU</td>
+                </tr>
+                <tr>
+                    <td align = right>RT/RW</td>
+                    <td colspan = 2>: 002/006</td>
+                </tr>
+                <tr>
+                    <td align = right>Kel/Desa</td>
+                    <td colspan = 2>: KANDANGAN</td>
+                </tr>
+                <tr>
+                    <td align = right>Kecamatan</td>
+                    <td colspan = 2>: KANDANGAN</td>
+                </tr>
+                <tr>
+                    <td>Agama</td>
+                    <td colspan = 2>: ISLAM</td>
+                </tr>
+                <tr>
+                    <td>Status Perkawinan</td>
+                    <td colspan = 2>: BELUM KAWIN</td>
+                </tr>
+                <tr>
+                    <td>Pekerjaan</td>
+                    <td colspan = 2>: MAHASISWA</td>
+                </tr>
+                <tr>
+                    <td>Kewarganegaraan</td>
+                    <td colspan = 2>: WNI</td>
+                </tr>
+                <tr>
+                    <td valign="top">Berlaku Hingga</td>
+                    <td colspan = 2 VALIGN="top">: SEUMUR HIDUP</td>
+                </tr>
+            </div>            
+        </table>
+    </body>
+</html>
